113eb195c08759fd60dac20ca20cd709385ffa5d
Guilherme Arcas <guilhermearcas1@gmail.com>
2026-06-03 11:20:58 -0400

    feat: Adicionando ferramentas de teste

--- biblioteca-backend/src/routes/bookRoutes.ts
+++ biblioteca-backend/src/routes/bookRoutes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import BookController from '../controllers/BookController';
 
 const router = Router();
-
+//
 router.post('/books', BookController.create);
 router.get('/books', BookController.list);
 
