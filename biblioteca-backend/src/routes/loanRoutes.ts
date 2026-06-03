79c48f756025d5d2453b75664a4cdeda8351c5fb
Guilherme Arcas <guilhermearcas1@gmail.com>
2026-06-03 18:16:39 -0400

    fix: arrumando

--- biblioteca-backend/src/routes/loanRoutes.ts
+++ biblioteca-backend/src/routes/loanRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import LoanController from '../controllers/LoanController';
+import { AuthMiddleware } from '../middlewares/authMiddleware';
 
 const router = Router();
 
-router.post('/loans', LoanController.create);
-router.post('/loans/return', LoanController.returnBook);
+router.post('/loans', LoanController.create, AuthMiddleware.handle);
+router.post('/loans/return',LoanController.returnBook);
 
 export default router;
